9d1a7a918b1a6d31b558034038457a75c9d6e226
sunil kumar <webdevlopersunil1997@gmail.com>
2024-01-01 15:20:07 +0530

    update Code

---
+++ MondayJAVAscript/index.js
@@ -0,0 +1,23 @@
+// let a=10;
+// let b= 20;
+// console.log(a+b);
+
+function check1(){
+
+let a =20;
+var b =40;
+const c =60;
+
+// console.log(a,b,c);
+
+if(a == 20){
+    let a =40;
+    var b =70;
+    const c= 80;
+    console.log(a,b,c);
+}
+
+console.log(a,b,c);
+
+}
+check1();
