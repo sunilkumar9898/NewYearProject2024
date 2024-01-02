39a83b445ecf3fb618d435c9b19394633957befc
sunil kumar <webdevlopersunil1997@gmail.com>
2024-01-02 10:50:47 +0530

    update Code

--- MondayJAVAscript/index.js
+++ MondayJAVAscript/index.js
@@ -2,22 +2,32 @@
 // let b= 20;
 // console.log(a+b);
 
-function check1(){
+// function check1(){
 
-let a =20;
-var b =40;
-const c =60;
+// let a =20;
+// var b =40;
+// const c =60;
+
+
+// if(a == 20){
+//     let a =40;
+//     var b =70;
+//     const c= 80;
+//     console.log(a,b,c);
+// }
 
 // console.log(a,b,c);
 
-if(a == 20){
-    let a =40;
-    var b =70;
-    const c= 80;
-    console.log(a,b,c);
-}
+// }
+// check1();
 
-console.log(a,b,c);
 
+let a = 40;
+if(a>61){
+console.log("you are Promoted")
+}else if(a<66 && a>45){
+    console.log("You are Middel");
 }
-check1();
+else{
+    console.log("you are not promoted");
+}
