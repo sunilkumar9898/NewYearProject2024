30b3606cac4aaee60df54449800cb377d023dde9
sunil kumar <webdevlopersunil1997@gmail.com>
2024-01-02 15:54:20 +0530

    update Code

---
+++ Date02.01.2024/index.js
@@ -0,0 +1,53 @@
+let a =40 ;
+if(a>=46){
+    console.log("you Are adult");
+}else if(a<=30){
+    console.log("you are most Valuable Person");
+}else if(a>50 || a<=40){
+    console.log("you are Very imoprtant person")
+}
+else{
+    console.log("ramu");
+}
+
+
+
+// Assigning values to variables
+
+var x = 0;
+var y = -1;
+var z = 4;
+if (x > y && x > z) {
+    if (y > z) {
+        console.log(x + ", " + y + ", " + z);
+    } else {
+        console.log(x + ", " + z + ", " + y);
+    }
+} else if (y > x && y > z) {
+    if (x > z) {
+        console.log(y + ", " + x + ", " + z);
+    } else {
+        console.log(y + ", " + z + ", " + x);
+    }
+} else if (z > x && z > y) {
+    if (x > y) {
+        console.log(z + ", " + x + ", " + y);
+    } else {
+        console.log(z + ", " + y + ", " + x);
+    }
+} 
+
+
+
+
+// var x = parseInt(prompt("Please enter your Marks"));
+//  if(x>=30 && x<60){
+//     console.log("You Are Passed By Second Division");
+//  }else if(x>=60 && x<80){
+//     console.log("You Are Passed By First Division");
+//  }else if(x>=80 && x<100){
+//     console.log("You Are Passed By Merrit");
+//  }
+//  else{
+//     console.log("Not eligible");
+//  }
