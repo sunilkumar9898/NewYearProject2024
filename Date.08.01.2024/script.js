e3b144a3d24012b32855529b3bde59e18332c104
sunil kumar <webdevlopersunil1997@gmail.com>
2024-01-08 17:46:03 +0530

    update 08-01-2024

--- Date.08.01.2024/script.js
+++ Date.08.01.2024/script.js
@@ -45,12 +45,36 @@
 // }
 
 
+// let i = 0;
+// let number = 10;
+// let sum = 0;
+// while (i<=number) {
+//     if(i%2==0){
+//         sum = sum +i;
+//     }
+//     i++
+// }
+// console.log(sum);
+
 // Q:2 Odd numbers
 // let a = 1;
 // while(a<=51){
 //     console.log(`The number is Odd ${a}`);
 //     a= a+2;
 // }
+
+
+
+// let i = 0;
+// let number = 9;
+// let odd_sum = 0;
+// while (i<=number) {
+//     if(i%2==1){
+//         odd_sum = odd_sum +i;
+//     }
+//     i++
+// }
+// console.log(odd_sum);
 
 
 // Q 3 : Sum of All even numbers
@@ -67,14 +91,14 @@
 
 // Q :4 Sum off All Odd Numbers
 
-let odd_number = 11;
-let value = 1;
-let odd_sum = 0;
-while (value<=odd_number) {
-    odd_sum = odd_sum+value;
-    value = value+2; 
-}
-console.log(odd_sum);
+// let odd_number = 11;
+// let value = 1;
+// let odd_sum = 0;
+// while (value<=odd_number) {
+//     odd_sum = odd_sum+value;
+//     value = value+2; 
+// }
+// console.log(odd_sum);
 
 
 
@@ -87,9 +111,63 @@ console.log(odd_sum);
 
 
 
+
+//     let i = 1;
+//     let number = 10;
+//     let Odd_sum = 0;
+//     while(i<=number){
+//         if(i%2==1){
+//         Odd_sum = Odd_sum + i;
+//     }
+//     i++;
+// }
+// console.log(Odd_sum)
+
+
+// let i =0;
+// let number = 10;
+// let sum_number = 0;
+// while(i<=number){
+//     if(i%2==0){
+//         sum_number = sum_number +i
+//     }
+//     i++
+// } 
+// console.log(sum_number);
+
+
+// function sum(num){
+//     let i = 0;
+//     // num=10;
+//     let sum_number = 0;
+//     while (i<=num) {
+//         if(i%2==0){
+//             sum_number = sum_number+i;
+//         }
+//         i++;
+//     }
+//     console.log(sum_number);
+// }
+// sum(10);
+
    
+// let i =1;
+// while (i<=10) {
+//     let result = 3*i;
+//     console.log(`3 * ${i} = ${result}`);
+//     i++; 
+// }
 
 
+function printTable(num){
+    let i =1;
+    while (i<=10) {
+        let result = num*i;
+        console.log(`${num} * ${i} = ${result}`);
+        i++; 
+    }
+}
+printTable(21);
 
 
 
