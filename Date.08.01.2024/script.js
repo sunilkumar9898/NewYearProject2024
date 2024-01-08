d2f6f1167735c5056105f3ea32b0a5eeefe1cbe0
sunil kumar <webdevlopersunil1997@gmail.com>
2024-01-08 16:30:45 +0530

    update 08-01-2024

---
+++ Date.08.01.2024/script.js
@@ -0,0 +1,97 @@
+// let a = 10;
+// let b = 20;
+// let c = 3;
+// let d = 40;
+// let max = Math.max(a,b,c,d)
+
+// if((a>b && a>c && a>d) || (b>a && b>c && b>d) || ( c>a && c>b && c>d) || (d>a && d>b && d>c)){
+//     console.log(max)
+// }else{
+//     console.log("Print Another Value");
+// }
+
+// if(a>b && a>b && a>c){
+//     console.log(a);
+// }else if(b>a && b>c && b>d){
+//     console.log(b);
+// }else if(c>a && c>b && c>d){
+//     console.log(c);
+// }else if(d>a && d>b && d>c){
+//     console.log(d);
+// }
+
+// let i=0;
+// while(i<=5){
+//     i=i+1
+// }
+// console.log(`the value of i is  ${i} `)
+
+// let i =0;
+// while(i<=5){
+//     i++;
+//     i=i+2;
+//     i--;
+//     i++;
+// }
+// console.log(i);
+
+
+
+// Q:1 Even numbers
+// let i =0;
+// while(i<=30){
+//     console.log(`The number is Even ${i}`);
+//     i=i+2;
+// }
+
+
+// Q:2 Odd numbers
+// let a = 1;
+// while(a<=51){
+//     console.log(`The number is Odd ${a}`);
+//     a= a+2;
+// }
+
+
+// Q 3 : Sum of All even numbers
+
+// let number = 10;
+// let x= 0;
+// let sum = 0;
+// while(x<=number){
+//     sum = sum+x;
+//     x=x+2;
+// }
+// console.log(sum);
+
+
+// Q :4 Sum off All Odd Numbers
+
+let odd_number = 11;
+let value = 1;
+let odd_sum = 0;
+while (value<=odd_number) {
+    odd_sum = odd_sum+value;
+    value = value+2; 
+}
+console.log(odd_sum);
+
+
+
+// let i = 1;
+// while (i<=10) {
+//     let result = 2*i
+//     console.log(`2 * ${i} = ${result}`);
+//     i++
+// }
+
+
+
+   
+
+
+
+
+
+
+
